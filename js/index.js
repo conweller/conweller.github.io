28eef9d733f888b435c17602eea858dc7d5edd5a
Connor Onweller <onweller@udel.edu>
2020-01-23 19:53:42 -0500

    Toggle-able navigation for mobile

--- js/index.js
+++ js/index.js
@@ -4,3 +4,24 @@ function goTo(id, route=`#${id}`) {
   url=url.split('#')[0]
   window.history.pushState("", "", `${url}${route}`);
 }
+
+function showNav() {
+  let list_elm = document.getElementById('nav-list');
+  if ( list_elm.style.display == 'flex' ) {
+    list_elm.style.display = 'none'
+  } else {
+    list_elm.style.display = 'flex'
+  }
+}
+
+window.addEventListener('resize', function(_){
+  let list_elm = document.getElementById('nav-list');
+  let icon_elm = document.getElementById('nav-icon');
+  if (window.innerWidth <= 600) {
+    list_elm.style.display = 'none'
+    icon_elm.style.display = 'flex'
+  } else {
+    icon_elm.style.display = 'none'
+    list_elm.style.display = 'flex'
+  }
+});
