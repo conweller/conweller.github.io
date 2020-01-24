9054657fb635460d85201579390e6c7702bd6196
Connor Onweller <onweller@udel.edu>
2020-01-23 20:05:55 -0500

    Hide menu when clicking on link or outside menu

--- js/index.js
+++ js/index.js
@@ -3,25 +3,39 @@ function goTo(id, route=`#${id}`) {
   url=document.location.href;
   url=url.split('#')[0]
   window.history.pushState("", "", `${url}${route}`);
+
+  let icon_elm = document.getElementById('nav-icon');
+  let list_elm = document.getElementById('nav-list');
+  if ( icon_elm.style.display == 'flex' && list_elm.style.display == 'flex' ) {
+    list_elm.style.display = 'none';
+  }
 }
 
 function showNav() {
   let list_elm = document.getElementById('nav-list');
   if ( list_elm.style.display == 'flex' ) {
-    list_elm.style.display = 'none'
+    list_elm.style.display = 'none';
   } else {
-    list_elm.style.display = 'flex'
+    list_elm.style.display = 'flex';
   }
 }
 
-window.addEventListener('resize', function(_){
+window.addEventListener('resize', (_) => {
   let list_elm = document.getElementById('nav-list');
   let icon_elm = document.getElementById('nav-icon');
   if (window.innerWidth <= 600) {
-    list_elm.style.display = 'none'
-    icon_elm.style.display = 'flex'
+    list_elm.style.display = 'none';
+    icon_elm.style.display = 'flex';
   } else {
-    icon_elm.style.display = 'none'
-    list_elm.style.display = 'flex'
+    icon_elm.style.display = 'none';
+    list_elm.style.display = 'flex';
+  }
+});
+
+document.addEventListener('click', (event) => {
+  let nav_elm = document.getElementsByTagName('nav')[0];
+  let list_elm = document.getElementById('nav-list');
+  if (!nav_elm.contains(event.target)) {
+    list_elm.style.display = 'none'
   }
 });
