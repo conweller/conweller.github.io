bbbe57c70d11e38c1b7b0e6a78582414999edc90
Connor Onweller <onweller@udel.edu>
2020-02-06 16:38:15 -0500

    Don't modify history

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function goTo(id, route=`#${id}`) {
   document.getElementById(id).scrollIntoView({behavior: "smooth"});
   url=document.location.href;
   url=url.split('#')[0]
-  window.history.pushState("", "", `${url}${route}`);
+  window.history.replaceState(null, "", `${url}${route}`);
 }
 
 function hideShowNav() {
